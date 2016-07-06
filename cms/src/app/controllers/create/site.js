bd2b6bb1d3dc745aaa943ebf4f56ed39b4c6a47f
linux-mint <treml.florian@gmx.de>
2016-07-06 16:15:31 +0200

    remove livereload; change render engine for site.js

--- cms/src/app/controllers/create/site.js
+++ cms/src/app/controllers/create/site.js
@@ -1,29 +1,30 @@
-var express = require('express'),
-  router = express.Router(),
-  mongoose = require('mongoose'),
-  Article = mongoose.model('Article');
+var express      = require('express');
+var router       = express.Router();
+var mongoose     = require('mongoose');
+var Article      = mongoose.model('Article');
 
 module.exports = function (app) {
   app.use('/', router);
 };
 
 router.get('/create/site', function (req, res, next) {
-  var article = new Article({
-    title : 'bli bla blub',
-    url   : '#',
-    text  : 'title text blibla blub fallerie fallera holahopsasa und so weiter und so fort'
-  });
-   
-  article.save(function (err, data) {
-  if (err) console.log(err);
-  else console.log('Saved : ', data );
-  });
-  Article.find(function (err, articles) {
-    if (err) return next(err);
-    console.log(articles);
-    res.render('create/site', {
-      title: 'CMS Create Site',
-      articles: articles
+    var article = new Article({
+        title : 'bli bla blub',
+        url   : '#',
+        text  : 'title text blibla blub fallerie fallera holahopsasa und so weiter und so fort'
     });
-  });
+console.log ("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    var templatePath = require.resolve('../../views/create/site.marko');
+    var template     = require('marko').load(templatePath);
+    // article.save(function (err, data) {
+    //     if (err) console.log(err);
+    //     else console.log('Saved : ', data );
+    // });
+    Article.find(function (err, articles) {
+        if (err) return next(err);
+        template.render({
+            title: 'CMS Create Site',
+            articles: articles
+        }, res);
+    });
 });
