56320d07929c41227bde0c7c9b3b15f80bfecb32
debugelton <treml.florian@gmx.de>
2016-07-10 16:21:01 +0200

    add redirect to post req

--- cms/src/app/controllers/create/site.js
+++ cms/src/app/controllers/create/site.js
@@ -1,8 +1,7 @@
-var express      = require('express');
-var router       = express.Router();
-var mongoose     = require('mongoose');
-var Article      = mongoose.model('Article');
-var Site      = mongoose.model('Site');
+var express  = require('express');
+var router   = express.Router();
+var mongoose = require('mongoose');
+var Site     = mongoose.model('Site');
 
 module.exports = function (app) {
   app.use('/', router);
@@ -11,11 +10,6 @@ module.exports = function (app) {
 router.get('/create/site', function (req, res, next) {
     var templatePath = req.app.get('views');
     var template = require(templatePath + '/create/site.marko');
-    // var site  = new Site({
-    //     title : 'bli bla blub',
-    //     url   : '#',
-    //     text  : 'title text blibla blub fallerie fallera holahopsasa und so weiter und so fort'
-    // });
     Site.find(function (err, sites) {
         if (err) return next(err);
         template.render({
@@ -31,22 +25,11 @@ router.get('/create/site', function (req, res, next) {
 
 
 router.post('/create/site', function (req, res, next) {
-    var templatePath = req.app.get('views');
-    var template = require(templatePath + '/create/site.marko');
     var params = req.body;
     var site  = new Site({
         title : params.title,
         route   : params.route
     });
     site.save();
-    template.render({
-        title: 'CMS Home',
-        params: params,
-        includes: {
-            main_menu: require(templatePath + "/components/link.main-menu.marko"),
-            header: require(templatePath + "/components/header/header.marko")
-        }
-    }, res);
-
-    // req.redirect(302, '/create/site');
+    res.redirect('/create/site');
 });
